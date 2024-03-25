16095358897100d32274d8ce121edc02b1c2acb9
Collin Stasiak <collinstasiak@gmail.com>
2024-03-24 22:03:45 -0400

    Adds helper methods

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rust HTTP Server
+Simple HTTP server because I wanted to learn how they work.
+I will incrementally add new features, but mainly ones I am interested in learning about
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Error;
+use std::fmt;
 use std::{
     io::{Read, Write},
     net::TcpListener,
@@ -11,11 +12,16 @@ pub struct TCPServer {
     pub port: String,
 }
 
-enum Status {
-    200,
-    403,
-    404,
-    500,
+pub enum Status {
+    OK = 200,
+    Forbidden = 403,
+    NotFound = 404,
+    InternalServerError = 500,
+}
+impl fmt::Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", &self)
+    }
 }
 
 impl TCPServer {
@@ -43,26 +49,46 @@ impl TCPServer {
     }
 
     fn handle_request(_data: &[u8]) -> Vec<u8> {
-        let response_line = "HTTP/1.1 200 OK \r\n".as_bytes();
-        let headers = [
-            "Server: Collin's based http server\r\n".as_bytes(),
-            "Content-Type: text/html\r\n".as_bytes(),
-        ]
-        .concat();
+        let response_line = Self::create_response_line(Status::OK);
+        let headers = Self::create_headers(&[
+            "Server: Collin's based http server\r\n",
+            "Content-Type: text/html\r\n",
+        ]);
         let blank_line = "\r\n".as_bytes();
         let response_body = "<html><body><h1>Message Recieved</h1></body></html>".as_bytes();
-        [response_line, &headers, blank_line, response_body].concat()
+        [&response_line, &headers, blank_line, response_body].concat()
     }
 
-    fn create_response_line(status: i32){
-        let reason = match status =>
-        {
-        (Status::200) => "Ok",
-        (Status::500) => "Internal Server Error",
-        (Status::404) => "Not Found",
+    fn create_headers(headers: &[&str]) -> Vec<u8> {
+        let mut binary_headers: Vec<u8> = vec![];
+        for header in headers {
+            binary_headers.extend(header.as_bytes());
+        }
+        binary_headers
     }
-        let response_line = format!("HTTP/1.1 {} {} \r\n",reason, status).as_bytes();
 
+    fn create_response_line(status: Status) -> Vec<u8> {
+        let reason = match &status {
+            Status::OK => "Ok",
+            Status::NotFound => "Not Found",
+            Status::Forbidden => "Forbidden",
+            Status::InternalServerError => "Internal Server Error",
+        };
+        format!("HTTP/1.1 {} {} \r\n", reason, status)
+            .as_bytes()
+            .to_vec()
+    }
+}
+
+struct HTTPRequest {
+    method: String,
+    uri: String,
+    http_version: i32,
+}
+
+impl HTTPRequest {
+    fn parse(request: &[&u8]) -> Self {
+        todo!();
     }
 }
 
